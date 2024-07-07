5e1a998525cf3e58fc8e95f7008c84f4d385833e
alazycoder <alazycoder101@gmail.com>
2024-07-08 00:53:16 +0100

    scafold command to generate notes

--- db/migrate/20240707234710_create_notes.rb
+++ db/migrate/20240707234710_create_notes.rb
@@ -1,8 +1,8 @@
 class CreateNotes < ActiveRecord::Migration[7.1]
   def change
     create_table :notes do |t|
-      t.string :title
-      t.string :content
+      t.string :title, null: false, limit: 250
+      t.string :content, null: false, limit: 1000
       t.timestamps
     end
   end
